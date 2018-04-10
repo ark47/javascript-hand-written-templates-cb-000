551ac24e8f1af6c1bedc54da1b6eb106b3c0f037
Chris Nunez <cenunez47@gmail.com>
2018-04-10 14:15:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function addComment() {
   event.preventDefault();
-  var bodyText = "This research is bold and important!";
-  var commenter = "Definitely not a woodchuck looking to eat the wood";
+  var bodyText = document.getElementById("commentText").value;
+  var commenter = document.getElementById("commenterName").value;
  
   var commentBody = createCommentBody(bodyText);
   var commenterLabel = createCommenterLabel(commenter);
